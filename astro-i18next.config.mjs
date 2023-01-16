edef56be22fefd73da744bd6317921aff88949c3
Facundo Garcia <zogar1993@gmail.com>
2023-01-16 18:42:52 -0300

    add personalized routes

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-  defaultLocale: "en-US",
-  locales: ["en-US", "es"],
-  i18nextServer: {
-    backend: {
-      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
-    },
-  },
-};
+	defaultLocale: "en-US",
+	locales: ["en-US", "es"],
+	i18nextServer: {
+		backend: {
+			loadPath: "./public/locales/{{lng}}/{{ns}}.json"
+		}
+	},
+
+	routes: {
+		es: {
+			"hello-world": "hola-mundo",
+			characters: {
+				index: "personajes"
+			}
+		}
+	}
+}
